7954822282ac02d7a5f8444646cac95652dbcc8f
Hardmun <spamnvg@gmail.com>
2021-03-03 02:48:23 +0300

    Lunamoon developing

--- excelFusion.py
+++ excelFusion.py
@@ -1,20 +1,21 @@
 import sys
-import time
 import os
+from openpyxl import load_workbook
 
 def runFusion():
     """global path"""
     projectDir = os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(
         os.path.abspath(__file__))
 
-    print("start sleeping")
-    time.sleep(10)
-    with open(os.path.join(projectDir, "lollol.txt"),'w') as myfile:
-        myfile.write("")
+    excel = load_workbook(os.path.join(projectDir, "test_files", "Ярославль.xlsx"))
+    sheet = excel.active
 
-    print("end slipping")
-    for i in range(1, len(sys.argv)):
-        print(argv[i])
+    excel.save(os.path.join(projectDir, "test_files", "Ярославль_remastered.xlsx"))
+    # with open(os.path.join(projectDir, "lollol.txt"),'w') as myfile:
+    #     myfile.write("")
+    #
+    # for i in range(1, len(sys.argv)):
+    #     print(argv[i])
 
 
 if __name__ == '__main__':
