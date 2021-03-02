c9c86d758bc1ab1ac560b7fa70c6c55a43bab420
Hardmun <spamnvg@gmail.com>
2021-03-03 02:18:35 +0300

    Lunamoon developing

--- excelFusion.py
+++ excelFusion.py
@@ -1,9 +1,20 @@
-from sys import argv
+import sys
+import time
+import os
 
 def runFusion():
-    for i in range(1, len(argv)):
-        print(argv[i])
+    """global path"""
+    projectDir = os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(
+        os.path.abspath(__file__))
 
+    print("start sleeping")
+    time.sleep(10)
+    with open(os.path.join(projectDir, "lollol.txt"),'w') as myfile:
+        myfile.write("")
+
+    print("end slipping")
+    for i in range(1, len(sys.argv)):
+        print(argv[i])
 
 if __name__ == '__main__':
     runFusion()
