ebcce4801d790cadfb158aad63d9ff41a0a1c539
Hardmun <spamnvg@gmail.com>
2021-03-04 04:13:28 +0300

    Lunamoon developing

--- com_excel.py
+++ com_excel.py
@@ -1,10 +1,17 @@
+import os
+import sys
+
 import win32com.client as win32
 
-
 def runFusion():
-    excel = win32.gencache.EnsureDispatch('Excel.Application')
+    """global path"""
+    projectDir = os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(
+        os.path.abspath(__file__))
 
-    excel.Application.Quit()
+    os.path.join(projectDir, "test_files", "ЯрославльCOM.xlsx")
+    # excel = win32.gencache.EnsureDispatch('Excel.Application')
+    #
+    # excel.Application.Quit()
 
 if __name__ == '__main__':
     runFusion()
