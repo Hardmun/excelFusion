c9c361b3732aec7ae4b2e6deb440d1470afbcad0
Hardmun <spamnvg@gmail.com>
2021-03-04 22:19:26 +0300

    Lunamoon developing

--- fusion.py
+++ fusion.py
@@ -1,28 +1,16 @@
 import os
 import sys
-from openpyxl import load_workbook
-from openpyxl import Workbook
-from openpyxl.utils import get_column_letter
-from xlwings import App
 
+from xlwings import App
 
 def runFusion():
     """global path"""
     projectDir = os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(
         os.path.abspath(__file__))
 
-    excelList = [os.path.join(projectDir, "test_files", "yar.xlsx"),
-                 os.path.join(projectDir, "test_files", "Client.xlsx")]
-
-    mainFile = ""
-    addFile = ""
-    # for file in excelList:
-
     app = App(visible=False)
     excel1 = app.books.open(os.path.join(projectDir, "test_files", "yar.xlsx"))
-    # excel2 = Book(os.path.join(projectDir, "test_files", "Челябинск.xlsx"))
 
-    # app = App(visible=False)
     excelCommon = app.books.open(os.path.join(projectDir, "test_files", "Client.xlsx"))
 
     ws1 = excel1.sheets(1)
@@ -31,7 +19,6 @@ def runFusion():
 
     excelCommon.save()
     excelCommon.app.quit()
-
 
 if __name__ == '__main__':
     runFusion()
