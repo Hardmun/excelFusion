0e097ce0773843b7c57d3851101d98e92670fdf0
Hardmun <spamnvg@gmail.com>
2021-03-04 23:03:34 +0300

    Lunamoon developing

--- combine.py
+++ combine.py
@@ -57,34 +57,38 @@ def combineFilesByopenpyxl():
 
 def copyCell():
     wb1 = load_workbook(os.path.join(projectDir, "test_files", "Client.xlsx"))
-    ws1 = wb1.create_sheet("lol")
+    target = wb1.create_sheet("lol")
 
     wb2 = load_workbook(os.path.join(projectDir, "test_files", "Ярославль_remastered.xlsx"))
-    ws2 = wb2.active
-    for row in ws2.iter_rows():
-        for cell in row:
-            try:
-                new_cell = ws1.cell(row=cell.row, column=cell.column, value=cell.value)
-                if cell.has_style:
-                    new_cell.font = copy(cell.font)
-                    new_cell.border = copy(cell.border)
-                    new_cell.fill = copy(cell.fill)
-                    new_cell.number_format = copy(cell.number_format)
-                    new_cell.protection = copy(cell.protection)
-                    new_cell.alignment = copy(cell.alignment)
-            except:
-                asdf = 0
-            if cell.has_style:
-                pass
-                # new_cell.font = cell.font
-                # new_cell.border = cell.border
-                # new_cell.fill = cell.fill
-                # new_cell.number_format = cell.number_format
-                # new_cell.protection = cell.protection
-                # new_cell.alignment = cell.alignment
+    source = wb2.active
 
-                # new_cell._style = copy(cell._style)
-        # ws1.append((cell.value for cell in row))
+    for (row, col), source_cell in source._cells.items():
+        target_cell = target.cell(column=col, row=row)
+        target_cell._value = source_cell._value
+        target_cell.data_type = source_cell.data_type
+
+        if source_cell.has_style:
+            target_cell.font = copy(source_cell.font)
+            target_cell.border = copy(source_cell.border)
+            target_cell.fill = copy(source_cell.fill)
+            target_cell.number_format = copy(source_cell.number_format)
+            target_cell.protection = copy(source_cell.protection)
+            target_cell.alignment = copy(source_cell.alignment)
+
+    for attr in ('row_dimensions', 'column_dimensions'):
+        src = getattr(source, attr)
+        trg = getattr(target, attr)
+        for key, dim in src.items():
+            trg[key] = copy(dim)
+            trg[key].worksheet = trg
+
+    target.sheet_format = copy(source.sheet_format)
+    target.sheet_properties = copy(source.sheet_properties)
+    target.merged_cells = copy(source.merged_cells)
+    target.page_margins = copy(source.page_margins)
+    target.page_setup = copy(source.page_setup)
+    target.print_options = copy(source.print_options)
+
 
     wb1.save(os.path.join(projectDir, "test_files", "fusion.xlsx"))
 
@@ -98,7 +102,7 @@ def copySheet():
     wb1.save(os.path.join(projectDir, "test_files", "fusion.xlsx"))
 
 if __name__ == '__main__':
-    copySheet()
-    # copyCell()
+    # copySheet()
+    copyCell()
     # combineFilesByopenpyxl()
     # combineFiles()
